6f6488ac8b92a9587a8ed926dbaa5f6a56ca5dd7
Gbaja <gbajaf@yahoo.co.uk>
2018-01-13 22:35:34 +0000

    basic functionality

--- build/public/index.js
+++ build/public/index.js
@@ -1,6 +1,59 @@
-"use strict";
+'use strict';
 
-var project = function project() {
-  console.log("hello");
+var start_game_btn = document.getElementById('start_game_btn');
+var list_container = document.getElementById('list_container');
+var guesses_input = document.getElementsByClassName('guesses_input');
+var gameview__hangman__bodyPart = document.getElementsByClassName('gameview__hangman__bodyPart');
+var live_para = document.getElementById('live_para');
+var gameover_para = document.getElementById('gameover_para');
+var playarea = document.getElementById('playarea');
+
+var word_to_guess = void 0;
+var num_of_lives = 0;
+
+var apiRequestForWord = function apiRequestForWord() {
+  return fetch("https://api.github.com/users/gbaja");
 };
-project();
+var requestResultAction = function requestResultAction() {
+  apiRequestForWord().then(function (response) {
+    return response.json();
+  }).then(function (response_text) {
+    return response_text.login;
+  }).then(function (word) {
+    word_to_guess = word.toUpperCase();
+    wordInput(word);
+  }).catch(function (err) {
+    console.error(err);
+  });
+};
+
+start_game_btn.addEventListener("click", requestResultAction);
+
+var wordInput = function wordInput(word) {
+  var word_array = word.split("");
+  word_array.map(function (letter) {
+    var input_element = document.createElement('input');
+    input_element.type = "text";
+    input_element.className = "guesses_input";
+    list_container.appendChild(input_element);
+  });
+};
+
+function alphabets_click_func(event) {
+  event.target.classList.add("used");
+  var letter = event.target.value;
+  var letterPosition = word_to_guess.indexOf(letter);
+  word_to_guess.indexOf(letter) === -1 ? (++num_of_lives, live_para.textContent = 'Used lives: ' + num_of_lives, console.log(gameview__hangman__bodyPart), gameview__hangman__bodyPart[num_of_lives - 1].style.display = "block", gameOver(num_of_lives)) : fillInAnswer(word_to_guess, letter);
+}
+
+var fillInAnswer = function fillInAnswer(placeholder_word, placeholder_letter) {
+  placeholder_word.split("").filter(function (each_letter, i) {
+    var letterPosition = "";
+    letterPosition = placeholder_word[i];
+    letterPosition === placeholder_letter ? guesses_input[i].value = placeholder_letter : false;
+  });
+};
+
+var gameOver = function gameOver(live) {
+  live === 6 ? (gameover_para.textContent = "GAME OVER! The word is " + word_to_guess, live_para.style.display = "none", playarea.style.display = "none") : false;
+};
--- public/index.js
+++ public/index.js
@@ -1,4 +1,63 @@
-const project = () => {
-  console.log("hello");
+const start_game_btn = document.getElementById('start_game_btn');
+const list_container = document.getElementById('list_container');
+const guesses_input = document.getElementsByClassName('guesses_input');
+const gameview__hangman__bodyPart = document.getElementsByClassName('gameview__hangman__bodyPart');
+const live_para = document.getElementById('live_para');
+const gameover_para = document.getElementById('gameover_para');
+const playarea = document.getElementById('playarea');
+
+let word_to_guess;
+let num_of_lives = 0;
+
+const apiRequestForWord = () => fetch("https://api.github.com/users/gbaja");
+const requestResultAction = () => {
+  apiRequestForWord().then((response) => {
+    return response.json();
+  }).then((response_text) => {
+    return response_text.login
+  }).then((word) => {
+    word_to_guess = word.toUpperCase()
+    wordInput(word)
+  }).catch((err) => {
+    console.error(err);
+  })
 }
-project()
+
+start_game_btn.addEventListener("click", requestResultAction)
+
+const wordInput = (word) => {
+  const word_array = word.split("");
+  word_array.map((letter) => {
+    const input_element = document.createElement('input');
+    input_element.type = "text";
+    input_element.className = "guesses_input";
+    list_container.appendChild(input_element);
+  })
+}
+
+function alphabets_click_func(event) {
+  event.target.classList.add("used")
+  const letter = event.target.value;
+  const letterPosition = word_to_guess.indexOf(letter);
+  word_to_guess.indexOf(letter) === -1 ? (
+    ++num_of_lives,
+    live_para.textContent = `Used lives: ${num_of_lives}`,
+    console.log(gameview__hangman__bodyPart),
+    gameview__hangman__bodyPart[num_of_lives-1].style.display = "block",
+    gameOver(num_of_lives)) : fillInAnswer(word_to_guess, letter);
+}
+
+const fillInAnswer = (placeholder_word, placeholder_letter) => {
+  placeholder_word.split("").filter((each_letter, i) => {
+    let letterPosition = "";
+    letterPosition = placeholder_word[i];
+    letterPosition === placeholder_letter ? guesses_input[i].value = placeholder_letter : false;
+
+  })
+}
+
+const gameOver = (live) => {
+  live === 6 ?  (gameover_para.textContent = "GAME OVER! The word is " + word_to_guess,
+  live_para.style.display = "none",
+  playarea.style.display = "none"): false;
+}
