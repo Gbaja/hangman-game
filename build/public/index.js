1e62393fc620f246a7fccb2a669c1a69c28da623
Gbaja <gbajaf@yahoo.co.uk>
2018-01-21 16:14:39 +0000

    Changed api request to https

--- build/public/index.js
+++ build/public/index.js
@@ -15,7 +15,7 @@ var word_to_guess = void 0;
 var num_of_lives = 0;
 
 var apiRequestForWord = function apiRequestForWord() {
-  return fetch('http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=691eca87b094010c8a00a0fa35204cc24a6f373ce37d18c5c');
+  return fetch('https://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=691eca87b094010c8a00a0fa35204cc24a6f373ce37d18c5c');
 };
 
 var requestResultAction = function requestResultAction() {
